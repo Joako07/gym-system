16599fdb2f31278ca097a77404bdbfe15748732c
Joaquin Alvarez <78284107+Joako07@users.noreply.github.com>
2025-08-09 19:44:21 -0300

    Category finalizado

--- microservices/product-microservices/src/main/java/com/joako/microservices/product_microservices/controllers/CategoryController.java
+++ microservices/product-microservices/src/main/java/com/joako/microservices/product_microservices/controllers/CategoryController.java
@@ -13,11 +13,12 @@ import lombok.RequiredArgsConstructor;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-
 
 @RestController
 @RequestMapping("api/v1/categories")
@@ -30,13 +31,24 @@ public class CategoryController {
     public ResponseEntity<List<CategoryResponse>> getAllCategories() {
         return ResponseEntity.ok(categoryServices.getAllCategories());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable Integer id) {
+        return ResponseEntity.ok(categoryServices.getCategoryById(id));
+    }
     
     @PostMapping()
     public ResponseEntity<Integer> createCategory(@Valid @RequestBody CategoryRequest request) {
         return ResponseEntity.ok(categoryServices.createCategory(request));
     }
-    
 
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponse> updateCategory(@PathVariable Integer id,@Valid @RequestBody CategoryRequest request) {
+        return ResponseEntity.ok(categoryServices.updateCategory(id,request));
+    }
 
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletCategory(@PathVariable Integer id){
+        return ResponseEntity.accepted().build();
+    }
 }
--- microservices/product-microservices/src/main/java/com/joako/microservices/product_microservices/services/CategoryServices.java
+++ microservices/product-microservices/src/main/java/com/joako/microservices/product_microservices/services/CategoryServices.java
@@ -11,21 +11,62 @@ import com.joako.microservices.product_microservices.models.responses.CategoryRe
 import com.joako.microservices.product_microservices.repositories.CategoryRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CategoryServices {
 
     private final CategoryRepository categoryRepository;
     private final CategoryMapper categoryMapper;
 
-    public List<CategoryResponse> getAllCategories(){
+    public List<CategoryResponse> getAllCategories() {
         return categoryRepository.findAll().stream().map(categoryMapper::entityToResponse).toList();
     }
 
-    public Integer createCategory(CategoryRequest request){
+    public CategoryResponse getCategoryById(Integer id) {
+
+        return categoryRepository
+                .findById(id)
+                .map(categoryMapper::entityToResponse)
+                .orElseThrow();
+    }
+
+    public Integer createCategory(CategoryRequest request) {
         CategoryEntity categoryEntity = categoryMapper.requestToEntity(request);
         return categoryRepository.save(categoryEntity).getId();
     }
 
+    public CategoryResponse updateCategory(Integer id, CategoryRequest request) {
+
+        CategoryEntity categoryEntity = categoryRepository.findById(id).orElseThrow();
+
+        categoryEntity.setId(id);
+        categoryEntity.setName(request.name());
+        categoryEntity.setDescription(request.description());
+
+        try {
+            CategoryEntity categoryEntity2 = categoryRepository.save(categoryEntity);
+            log.info("A Category was updated");
+            return categoryMapper.entityToResponse(categoryEntity2);
+        } catch (Exception e) {
+            log.error("An error occurred while updating a Category", e);
+            return null;
+        }
+    }
+
+    public void deletCategory(Integer id) {
+
+        if (!categoryRepository.existsById(id)) {
+            //poner exception 
+        } else {
+            try {
+                categoryRepository.deleteById(id);
+                log.info("A Category was deleted");
+            } catch (Exception e) {
+                log.error("An error occurred while deleting a Category", e);
+            }
+        }
+    }
 }
