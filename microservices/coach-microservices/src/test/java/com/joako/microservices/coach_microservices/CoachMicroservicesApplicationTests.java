95acdffa2cba0400c447e53debe441fb5a532132
Joaquin Alvarez <78284107+Joako07@users.noreply.github.com>
2025-09-01 13:39:58 -0300

    Inicio el servicio de coach

---
+++ microservices/coach-microservices/src/test/java/com/joako/microservices/coach_microservices/CoachMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joako.microservices.coach_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
