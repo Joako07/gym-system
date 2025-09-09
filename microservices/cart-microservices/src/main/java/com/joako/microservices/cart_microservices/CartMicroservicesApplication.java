90ebcb8573049dd34825c01846cd8d00f1573918
Joaquin Alvarez <78284107+Joako07@users.noreply.github.com>
2025-09-08 21:19:02 -0300

    Iniciando servicio de carrito

---
+++ microservices/cart-microservices/src/main/java/com/joako/microservices/cart_microservices/CartMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.joako.microservices.cart_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartMicroservicesApplication.class, args);
+	}
+
+}
